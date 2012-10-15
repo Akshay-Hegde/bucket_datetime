3922c7c4caec3b0f75084aa3eeeb71d73e5f1770
Ryan Thompson <service@aiwebsystems.com>
2012-10-15 00:29:46 -0500

    Initial commit

---
+++ language/english/bucket_datetime_lang.php
@@ -0,0 +1,19 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+$lang['streams.bucket_datetime.name'] 						= 'Bucket Date/Time';
+$lang['streams.bucket_datetime.use_time'] 					= 'Use Time';
+$lang['streams.bucket_datetime.timezone'] 					= 'Timezone';
+$lang['streams.bucket_datetime.start_date'] 				= 'Start Restriction';
+$lang['streams.bucket_datetime.end_date'] 					= 'End Restriction';
+$lang['streams.bucket_datetime.rest_instructions'] 			= 'Takes numerical offset from current date (Ex: -20) or numerical offset for days (D), months (M), and years (Y). Ex: +1D +2Y<';
+$lang['streams.bucket_datetime.storage']					= 'Storage Format';
+$lang['streams.bucket_datetime.input_type']					= 'Input Type';
+$lang['streams.invalid_input_for_date_range_check'] 		= 'Invalid date input for data range check.';
+
+// Buckets
+$lang['streams.bucket_datetime.option_today']				= 'Today';
+$lang['streams.bucket_datetime.option_tomorrow']			= 'Tomorrow';
+$lang['streams.bucket_datetime.option_this_week']			= 'This Week';
+$lang['streams.bucket_datetime.option_next_week']			= 'Next Week';
+$lang['streams.bucket_datetime.option_later']				= 'Later';
+$lang['streams.bucket_datetime.option_specific']			= 'Specific date/time...';
