b36df6065a470a61d11aec2ce0a805010c810ae5
Jacob Albert Jolman <jaap@jolman.eu>
2013-01-21 19:54:22 +0100

    Dutch Language Added

---
+++ language/dutch/bucket_datetime_lang.php
@@ -0,0 +1,19 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+$lang['streams.bucket_datetime.name'] 						= 'Emmer Datum/Tijd';
+$lang['streams.bucket_datetime.use_time'] 					= 'Gebruik Tijd';
+$lang['streams.bucket_datetime.timezone'] 					= 'Tijdzone';
+$lang['streams.bucket_datetime.start_date'] 				= 'Start Restictie';
+$lang['streams.bucket_datetime.end_date'] 					= 'Ende restrictie';
+$lang['streams.bucket_datetime.rest_instructions'] 			= 'Gebruikt nummerieke waarde van de huidige datum (bvb: -20) of nummerieke waarde voor aantal dagen (D), maanden (M), en jaren (Y). bvb: +1D +2Y<';
+$lang['streams.bucket_datetime.storage']					= 'Opslag Formaat';
+$lang['streams.bucket_datetime.input_type']					= 'Invoer Type';
+$lang['streams.invalid_input_for_date_range_check'] 		= 'Ongeldige datum voor datum bereik controle.';
+
+// Buckets
+$lang['streams.bucket_datetime.option_today']				= 'Vandaag';
+$lang['streams.bucket_datetime.option_tomorrow']			= 'Morgen';
+$lang['streams.bucket_datetime.option_this_week']			= 'Deze Week';
+$lang['streams.bucket_datetime.option_next_week']			= 'Volgende Week';
+$lang['streams.bucket_datetime.option_later']				= 'Later';
+$lang['streams.bucket_datetime.option_specific']			= 'Specifieke datum/tijd...';
